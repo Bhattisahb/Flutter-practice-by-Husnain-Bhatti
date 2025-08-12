499eb1cd1aaebf001dfe46d782b34571659499be
Bhattisahb <146937860+Bhattisahb@users.noreply.github.com>
2025-08-11 21:59:18 -0700

    video 1
    Blank project created

---
+++ bmi_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+
+main(){
+  
+     runApp(MyApp());
+  
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+class BMI extends StatefulWidget {
+  const BMI({super.key});
+
+  @override
+  State<BMI> createState() => _BMIState();
+}
+
+class _BMIState extends State<BMI> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
